3a8420c09321e0f44a647afa542fe875c1077618
樵夫砍柴 <56510104+201816040319@users.noreply.github.com>
2023-09-06 17:04:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oNZjv6hZY0-JrSYgO01KWZUvuFk4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'XFmzDSSiHnjwpOSHcLplkbvhym3-CUIfqZmIWDeXBSw',
+      useTemplateId: '6vfybvpyQoFKRyxdHbDnhusK6kaM1NHtKqSwLxsCok0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-26',
       festivals: [
@@ -44,16 +44,16 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2023-03-1' },
       ],
-    },
+    }, 
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '6vfybvpyQoFKRyxdHbDnhusK6kaM1NHtKqSwLxsCok0',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '周某某',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oNZjv6poKe32LpiPacCshLPYBMfc',
     }
