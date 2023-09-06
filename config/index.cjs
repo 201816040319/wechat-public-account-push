ef5f43aad0e04548c30c364c19ac26461195a0ba
樵夫砍柴 <56510104+201816040319@users.noreply.github.com>
2023-09-06 17:14:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oNZjv6hZY0-JrSYgO01KWZUvuFk4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '6vfybvpyQoFKRyxdHbDnhusK6kaM1NHtKqSwLxsCok0',
+      useTemplateId: 'VSUORG6C8Hr1RUZ_FrluSMvE67x_tfqGKLcU0RFBVFs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-26',
       festivals: [
